bf374b658043dacbb0aea938b3a5f3eaae71413c
Hyrum Durfee <htdurfee@byu.edu>
2024-03-18 14:24:41 -0600

    Budgie - Messaging and editing budget names implemented

--- budgie/group.js
+++ budgie/group.js
@@ -1,9 +1,8 @@
 //Feature: Assign a random color to the name of each person messaging
 
-//TODO: Add a way to edit budget names
 //TODO: Add an edit budget name button next to the View button
+//TODO: Add a delete budget button next to the Edit button
 //TODO: Don't allow duplicate budget names
-//TODO: Change the login button to be a window.location.href instead of a hyperlink
 
 
 let user = localStorage.getItem("currentUser");
@@ -46,7 +45,7 @@ function loadBudget(budget) {
     let buttonContainer = document.createElement("div");
     buttonContainer.className = "budget-info-buttons-container";
 
-    let privacyButtonContainer = document.createElement("div");
+    let privacySelectionContainer = document.createElement("div");
 
     let privacySelection = document.createElement("select");
     privacySelection.className = "form-select";
@@ -61,21 +60,38 @@ function loadBudget(budget) {
     publicOption.textContent = "Public";
     publicOption.selected = (budget.privacy === "public") ? 1 : 0;
     privacySelection.appendChild(publicOption);
-    privacyButtonContainer.appendChild(privacySelection);
-    buttonContainer.appendChild(privacyButtonContainer);
+    privacySelectionContainer.appendChild(privacySelection);
+    buttonContainer.appendChild(privacySelectionContainer);
 
     let miniSpace = document.createElement("div");
     miniSpace.className = "tiny-filler";
     buttonContainer.appendChild(miniSpace);
-
-    let viewButtonContainer = document.createElement("div");
     
     let viewButton = document.createElement("button");
     viewButton.className = "btn btn-light";
     viewButton.textContent = "View";
-    viewButton.addEventListener("click", () => {view(viewButton)});
-    viewButtonContainer.appendChild(viewButton);
-    buttonContainer.appendChild(viewButtonContainer);
+    viewButton.addEventListener("click", () => {viewBudget(viewButton)});
+    buttonContainer.appendChild(viewButton);
+
+    miniSpace = document.createElement("div");
+    miniSpace.className = "tiny-filler";
+    buttonContainer.appendChild(miniSpace);
+
+    let editButton = document.createElement("button");
+    editButton.className = "btn btn-light";
+    editButton.textContent = "Edit";
+    editButton.addEventListener("click", () => {editName(editButton)});
+    buttonContainer.appendChild(editButton);
+
+    miniSpace = document.createElement("div");
+    miniSpace.className = "tiny-filler";
+    buttonContainer.appendChild(miniSpace);
+    
+    let deleteButton = document.createElement("button");
+    deleteButton.className = "btn btn-light";
+    deleteButton.textContent = "Delete";
+    deleteButton.addEventListener("click", () => {deleteBudget(deleteButton)});
+    buttonContainer.appendChild(deleteButton);
 
     row.appendChild(buttonContainer);
 
@@ -88,7 +104,25 @@ function savePrivacy(selectElement) {
     let budgetName = selectElement.parentElement.parentElement.parentElement.querySelector(".info-title").textContent;
     let budgetElement = parseBudget(budgetName);
     budgetElement.privacy = selectElement.value.toLowerCase();
+    saveBudget(budgetElement, budgetName);
+}
 
+function viewBudget(buttonElement) {
+    //Change the selected budget to be the one selected, and go to the page with that budget loaded in
+    let budgetName = buttonElement.parentElement.parentElement.parentElement.querySelector(".info-title").textContent;
+    localStorage.setItem("currentBudget", budgetName);
+    window.location.href = "projected.html";
+}
+
+function parseBudget(budgetName) {
+    for (let i = 0; i < currUser.budgets.length; i++) {
+        if (currUser.budgets[i].budgetName === budgetName) {
+            return currUser.budgets[i];
+        }
+    }
+}
+
+function saveBudget(budgetElement, budgetName) {
     for (let i = 0; i < currUser.budgets.length; i++) {
         if (currUser.budgets[i].budgetName === budgetName) {
             currUser.budgets[i] = budgetElement;
@@ -103,21 +137,6 @@ function savePrivacy(selectElement) {
         }
     }
     localStorage.setItem("users", JSON.stringify(users));
-}
-
-function view(buttonElement) {
-    //Change the selected budget to be the one selected, and go to the page with that budget loaded in
-    let budgetName = buttonElement.parentElement.parentElement.parentElement.querySelector(".info-title").textContent;
-    localStorage.setItem("currentBudget", budgetName);
-    window.location.href = "projected.html";
-}
-
-function parseBudget(budgetName) {
-    for (let i = 0; i < currUser.budgets.length; i++) {
-        if (currUser.budgets[i].budgetName === budgetName) {
-            return currUser.budgets[i];
-        }
-    }
 }
 
 function newBudget() {
@@ -139,6 +158,44 @@ function newBudget() {
     loadBudgets();
     localStorage.setItem("currentBudget", budgetName);
     window.location.href = "projected.html";
+}
+
+function editName(editButton) {
+    //Input the new budget name. Find the budget corresponding to that button. Edit the name in localStorage. Call load.
+    let budgetInfoContainer = editButton.parentElement.parentElement;
+    let oldBudgetName = budgetInfoContainer.querySelector(".info-title").textContent;
+    console.log(oldBudgetName)
+    let newBudgetName = prompt("Enter budget name:");
+    if (newBudgetName === "" || newBudgetName === null) return;
+    let budgetElement = parseBudget(oldBudgetName);
+    budgetElement.budgetName = newBudgetName;
+    saveBudget(budgetElement, oldBudgetName);
+    loadBudgets();
+}
+
+function deleteBudget(deleteButton) {
+    console.log("deleteBudget called");
+}
+
+function sendMessage() {
+    let messageContainer = document.querySelector(".messages");
+    
+    let thisMessageContainer = document.createElement("div");
+
+    let messageTitle = document.createElement("span");
+    messageTitle.textContent = "You: ";
+    thisMessageContainer.appendChild(messageTitle);
+
+    let inputtedMessage = document.querySelector("#response").value;
+    let newMessage = document.createElement("span");
+    newMessage.textContent = inputtedMessage;
+    thisMessageContainer.appendChild(newMessage);
+
+    messageContainer.appendChild(thisMessageContainer);
+    
+    document.querySelector("#response").value = "";
+    
+    //TODO: Save messages sent
 }
 
 function logout() {
