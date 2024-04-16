63caedd6f44d1a3bdda5b4abf7753e628c582a70
Hyrum Durfee <htdurfee@byu.edu>
2024-04-16 01:23:06 -0600

    Budgie - DB functions getAuthData and createUserData

--- budgie/database.js
+++ budgie/database.js
@@ -1,16 +1,18 @@
+//Feature: Upon getting authData, delete any expired authData associated with that username
+//Feature: Hash passwords using bCrypt
 const {MongoClient} = require('mongodb');
 const config = require('./dbConfig.json');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
 const db = client.db('budgie');
-const users = client.db('users');
+const authData = db.collection("authData");
+const userData = db.collection("userData");
 
 async function testConnection() {
     try {
         await client.connect();
         await db.command({ping: 1});
-        const result = await db.collection("testCollection").insertOne({"test": "tacos"});
         console.log("Success");
     }
     catch(exception) {
@@ -19,4 +21,38 @@ async function testConnection() {
     }
 }
 
-testConnection();
+async function getAuthData(authToken) {
+    //Will return null if nothing matches the query
+    const result = await authData.findOne({authToken: authToken});
+    console.log(result);
+}
+
+async function createUserData(username, password, authToken, userData) {
+    const result = await authData.findOne({username: username});
+    if (result) return "userExists";
+    else {
+        await authData.insertOne({
+            username: username,
+            password: password,
+            authToken: authToken
+        });
+        await userData.insertOne({
+            user: userData
+        });
+    }
+}
+
+async function updateUserData() {
+
+}
+
+async function getUserData() {
+
+}
+
+
+
+module.exports = {
+    getAuthData,
+    createUserData
+}
